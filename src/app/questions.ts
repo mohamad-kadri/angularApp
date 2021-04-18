8c0ee229bc19dbc775e33dc28a8b8763b4de282a
Mohamad <mohd.elkadri@gmail.com>
2021-04-18 16:38:48 +0300

    1st commit

--- src/app/questions.ts
+++ src/app/questions.ts
@@ -1,17 +1,27 @@
 export const questions = [
   {
     id: 1,
-    question: 'What is my favorite hobby?',
-    answer: 'It is coding in Angular!'
+    question: 'What is ecomz for?',
+    answer: 'ecomz is an all-in-one scalable and user-friendly e-commerce platform. It will help you build your online store, grow your revenue stream and rank better on search result pages.'
   },
   {
     id: 2,
-    question: 'How is the weather today?',
-    answer: 'It is sunny'
+    question: 'How do I get started?',
+    answer: 'One word: Register! To register an account on ecomz, all you have to do is to fill a registration form. Once you do, you will be able to enjoy our e-commerce platform and launch your new journey.'
   },
   {
     id: 3,
-    question: 'Do I prefer Windows or Mac?',
-    answer: 'Both'
+    question: 'Can ecomz help me set up my store?',
+    answer: 'Absolutely! Ecomz experts are quite handy when it comes to the store setup, you can contact them for more details!'
+  },
+  {
+    id: 4,
+    question: 'Does ecomz offer free themes?',
+    answer: 'We surely do! You can visit the theme store on ecomz and see for yourself.'
+  },
+  {
+    id: 5,
+    question: 'Do all plans benefit from the 24/7 support?',
+    answer: 'Of course, we’ve got everyone’s back!'
   }
 ];
